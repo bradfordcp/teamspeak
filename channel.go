0c5d5815aeb449a274dbfc5a0684b1ce64f3f892
Christopher Bradford <bradfordcp@gmail.com>
2014-05-04 00:26:23 -0700

    Added ChannelInfo call to teamspeak.Conn
    Added additional properties to channels
    Added Deserialize method to channel
    Replaced strings.Split with strings.SplitN in Deserialize method. Fixes
    subtle bug with attributes containing ‘=‘ signs.

--- channel.go
+++ channel.go
@@ -8,58 +8,258 @@ import (
 )
 
 type Channel struct {
+	// Retrieved in ChannelList call
 	Cid                         uint
 	Pid                         uint
 	ChannelOrder                uint
 	ChannelName                 string
 	TotalClients                uint
 	ChannelNeededSubscribePower uint
+
+	// Retrieved in ChannelInfo call
+	ChannelTopic                         string
+	ChannelDescription                   string
+	ChannelPassword                      string
+	ChannelCodec                         uint
+	ChannelCodecQuality                  uint
+	ChannelMaxClients                    int
+	ChannelMaxFamilyClients              int
+	ChannelFlagPermanent                 bool
+	ChannelFlagSemiPermanent             bool
+	ChannelFlagDefault                   bool
+	ChannelFlagPassword                  bool
+	ChannelCodecLatencyFactor            uint
+	ChannelCodecIsUnencrypted            bool
+	ChannelSecuritySalt                  string
+	ChannelDeleteDelay                   uint
+	ChannelFlagMaxClientsUnlimited       bool
+	ChannelFlagMaxFamilyClientsUnlimited bool
+	ChannelFlagMaxFamilyClientsInherited bool
+	ChannelFilepath                      string
+	ChannelNeededTalkPower               uint
+	ChannelForcedSilence                 bool
+	ChannelNamePhonetic                  string
+	ChannelIconId                        int
+	ChannelFlagPrivate                   bool
+	SecondsEmpty                         int
 }
 
 func NewChannel(channelStr string) (*Channel, error) {
-	// Instantiate the channel we are going to return
+	// Instantiate the Channel
 	channel := &Channel{}
 
-	// Split the tokens and fill in our channel
-	tokens := strings.Split(channelStr, " ")
-	for _, token := range tokens {
-		attribute := strings.Split(token, "=")
-		switch attribute[0] {
-		case "cid":
-			cid, err := strconv.ParseUint(attribute[1], 10, 32)
-			if err != nil {
-				return channel, err
-			}
-			channel.Cid = uint(cid)
-		case "pid":
-			pid, err := strconv.ParseUint(attribute[1], 10, 32)
-			if err != nil {
-				return channel, err
-			}
-			channel.Pid = uint(pid)
-		case "channel_order":
-			channelOrder, err := strconv.ParseUint(attribute[1], 10, 32)
-			if err != nil {
-				return channel, err
-			}
-			channel.ChannelOrder = uint(channelOrder)
-		case "channel_name":
-			channel.ChannelName = attribute[1]
-		case "total_clients":
-			totalClients, err := strconv.ParseUint(attribute[1], 10, 32)
-			if err != nil {
-				return channel, err
-			}
-			channel.TotalClients = uint(totalClients)
-		case "channel_needed_subscribe_power":
-			channelNeededSubscribePower, err := strconv.ParseUint(attribute[1], 10, 32)
-			if err != nil {
-				return channel, err
-			}
-			channel.ChannelNeededSubscribePower = uint(channelNeededSubscribePower)
+	// Update the properties with the passed in data
+	channel.Deserialize(channelStr)
 
-		default:
-			return channel, errors.New(fmt.Sprintf("Error invalid parameter detected (%v) from %v", attribute[0], channelStr))
+	// Sanity check on the channel
+	if channel.Cid == 0 {
+		return channel, errors.New(fmt.Sprintf("Invalid channel, no cid present from %v", channelStr))
+	}
+
+	return channel, nil
+}
+
+// Update the properties of the channel with the attributes passed in
+func (channel *Channel) Deserialize(propertiesStr string) (*Channel, error) {
+	// Split the tokens and fill in our channel
+	tokens := strings.Split(propertiesStr, " ")
+	for _, token := range tokens {
+		attribute := strings.SplitN(token, "=", 2)
+
+		if len(attribute) == 2 {
+			switch attribute[0] {
+			case "cid":
+				cid, err := strconv.ParseUint(attribute[1], 10, 32)
+				if err != nil {
+					return channel, err
+				}
+				channel.Cid = uint(cid)
+
+			case "pid":
+				pid, err := strconv.ParseUint(attribute[1], 10, 32)
+				if err != nil {
+					return channel, err
+				}
+				channel.Pid = uint(pid)
+
+			case "channel_order":
+				channelOrder, err := strconv.ParseUint(attribute[1], 10, 32)
+				if err != nil {
+					return channel, err
+				}
+				channel.ChannelOrder = uint(channelOrder)
+
+			case "channel_name":
+				channel.ChannelName = attribute[1]
+
+			case "total_clients":
+				totalClients, err := strconv.ParseUint(attribute[1], 10, 32)
+				if err != nil {
+					return channel, err
+				}
+				channel.TotalClients = uint(totalClients)
+
+			case "channel_needed_subscribe_power":
+				channelNeededSubscribePower, err := strconv.ParseUint(attribute[1], 10, 32)
+				if err != nil {
+					return channel, err
+				}
+				channel.ChannelNeededSubscribePower = uint(channelNeededSubscribePower)
+
+			case "channel_topic":
+				channel.ChannelTopic = attribute[1]
+
+			case "channel_description":
+				channel.ChannelDescription = attribute[1]
+
+			case "channel_password":
+				channel.ChannelPassword = attribute[1]
+
+			case "channel_codec":
+				channelCodec, err := strconv.ParseUint(attribute[1], 10, 32)
+				if err != nil {
+					return channel, err
+				}
+				channel.ChannelCodec = uint(channelCodec)
+
+			case "channel_codec_quality":
+				channelCodecQuality, err := strconv.ParseUint(attribute[1], 10, 32)
+				if err != nil {
+					return channel, err
+				}
+				channel.ChannelCodecQuality = uint(channelCodecQuality)
+
+			case "channel_maxclients":
+				channelMaxClients, err := strconv.ParseInt(attribute[1], 10, 32)
+				if err != nil {
+					return channel, err
+				}
+				channel.ChannelMaxClients = int(channelMaxClients)
+
+			case "channel_maxfamilyclients":
+				channelMaxFamilyClients, err := strconv.ParseInt(attribute[1], 10, 32)
+				if err != nil {
+					return channel, err
+				}
+				channel.ChannelMaxFamilyClients = int(channelMaxFamilyClients)
+
+			case "channel_flag_permanent":
+				channelFlagPermanent, err := strconv.ParseBool(attribute[1])
+				if err != nil {
+					return channel, err
+				}
+				channel.ChannelFlagPermanent = bool(channelFlagPermanent)
+
+			case "channel_flag_semi_permanent":
+				channelFlagSemiPermanent, err := strconv.ParseBool(attribute[1])
+				if err != nil {
+					return channel, err
+				}
+				channel.ChannelFlagSemiPermanent = bool(channelFlagSemiPermanent)
+
+			case "channel_flag_default":
+				channelFlagDefault, err := strconv.ParseBool(attribute[1])
+				if err != nil {
+					return channel, err
+				}
+				channel.ChannelFlagDefault = bool(channelFlagDefault)
+
+			case "channel_flag_password":
+				channelFlagPassword, err := strconv.ParseBool(attribute[1])
+				if err != nil {
+					return channel, err
+				}
+				channel.ChannelFlagPassword = bool(channelFlagPassword)
+
+			case "channel_codec_latency_factor":
+				channelCodecLatencyFactor, err := strconv.ParseUint(attribute[1], 10, 32)
+				if err != nil {
+					return channel, err
+				}
+				channel.ChannelCodecLatencyFactor = uint(channelCodecLatencyFactor)
+
+			case "channel_codec_is_unencrypted":
+				channelCodecIsUnencrypted, err := strconv.ParseBool(attribute[1])
+				if err != nil {
+					return channel, err
+				}
+				channel.ChannelCodecIsUnencrypted = bool(channelCodecIsUnencrypted)
+
+			case "channel_security_salt":
+				channel.ChannelSecuritySalt = attribute[1]
+
+			case "channel_delete_delay":
+				channelDeleteDelay, err := strconv.ParseUint(attribute[1], 10, 32)
+				if err != nil {
+					return channel, err
+				}
+				channel.ChannelDeleteDelay = uint(channelDeleteDelay)
+
+			case "channel_flag_maxclients_unlimited":
+				channelFlagMaxClientsUnlimited, err := strconv.ParseBool(attribute[1])
+				if err != nil {
+					return channel, err
+				}
+				channel.ChannelFlagMaxClientsUnlimited = bool(channelFlagMaxClientsUnlimited)
+
+			case "channel_flag_maxfamilyclients_unlimited":
+				channelFlagMaxFamilyClientsUnlimited, err := strconv.ParseBool(attribute[1])
+				if err != nil {
+					return channel, err
+				}
+				channel.ChannelFlagMaxFamilyClientsUnlimited = bool(channelFlagMaxFamilyClientsUnlimited)
+
+			case "channel_flag_maxfamilyclients_inherited":
+				channelFlagMaxFamilyClientsInherited, err := strconv.ParseBool(attribute[1])
+				if err != nil {
+					return channel, err
+				}
+				channel.ChannelFlagMaxFamilyClientsInherited = bool(channelFlagMaxFamilyClientsInherited)
+
+			case "channel_filepath":
+				channel.ChannelFilepath = attribute[1]
+
+			case "channel_needed_talk_power":
+				channelNeededTalkPower, err := strconv.ParseUint(attribute[1], 10, 32)
+				if err != nil {
+					return channel, err
+				}
+				channel.ChannelNeededTalkPower = uint(channelNeededTalkPower)
+
+			case "channel_forced_silence":
+				channelForcedSilence, err := strconv.ParseBool(attribute[1])
+				if err != nil {
+					return channel, err
+				}
+				channel.ChannelForcedSilence = bool(channelForcedSilence)
+
+			case "channel_name_phonetic":
+				channel.ChannelNamePhonetic = attribute[1]
+
+			case "channel_icon_id":
+				channelIconId, err := strconv.ParseInt(attribute[1], 10, 32)
+				if err != nil {
+					return channel, err
+				}
+				channel.ChannelIconId = int(channelIconId)
+
+			case "channel_flag_private":
+				channelFlagPrivate, err := strconv.ParseBool(attribute[1])
+				if err != nil {
+					return channel, err
+				}
+				channel.ChannelFlagPrivate = bool(channelFlagPrivate)
+
+			case "seconds_empty":
+				secondsEmpty, err := strconv.ParseInt(attribute[1], 10, 32)
+				if err != nil {
+					return channel, err
+				}
+				channel.SecondsEmpty = int(secondsEmpty)
+
+			default:
+				return channel, errors.New(fmt.Sprintf("Error invalid parameter detected (%v) from %v", attribute[0], propertiesStr))
+			}
 		}
 	}
 
@@ -70,7 +270,6 @@ func NewChannel(channelStr string) (*Channel, error) {
 func (ts3 *Conn) ChannelList() ([]*Channel, error) {
 	response, err := ts3.SendCommand("channellist")
 	if ts3Err, ok := err.(*Error); ok && ts3Err.Id == 0 {
-
 		// Split the channel data on the | character
 		rawChannels := strings.Split(response, "|")
 		channels := make([]*Channel, len(rawChannels))
@@ -89,4 +288,17 @@ func (ts3 *Conn) ChannelList() ([]*Channel, error) {
 
 	empty := make([]*Channel, 0)
 	return empty, err
+}
+
+func (ts3 *Conn) ChannelInfo(channel *Channel) error {
+	response, err := ts3.SendCommand(fmt.Sprintf("channelinfo cid=%d", channel.Cid))
+	if ts3Err, ok := err.(*Error); ok && ts3Err.Id == 0 {
+		_, err := channel.Deserialize(response)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
--- channel_test.go
+++ channel_test.go
@@ -4,23 +4,59 @@ import (
 	"testing"
 )
 
-func TestNewChannel(t *testing.T) {
-	const validChannelString = "cid=1 pid=2 channel_order=3 channel_name=Sample\\sChannel\\sName total_clients=4 channel_needed_subscribe_power=5"
-	const invalidChannelString = "I'm not a channel, nice try!"
+const validChannelPropertyString = "cid=1 pid=2 channel_order=3 channel_name=Sample\\sChannel\\sName total_clients=4 channel_needed_subscribe_power=5"
+const validChannelPropertyStringWithNull = "cid=1 pid channel_order=3 channel_name=Sample\\sChannel\\sName total_clients=4 channel_needed_subscribe_power=5"
+const invalidChannelPropertyString = "I'm not a channel, nice try!"
 
+func TestNewChannel(t *testing.T) {
 	// Test to see if a valid channel string is converted into a Channel struct
-	validChannel, err := NewChannel(validChannelString)
+	validChannel, err := NewChannel(validChannelPropertyString)
 	if err != nil {
-		t.Errorf("NewChannel(\"%v\"): Errored out with %v", validChannelString, err)
+		t.Errorf("NewChannel(\"%v\"): Errored out with %v", validChannelPropertyString, err)
 	} else {
 		if validChannel.Cid != 1 || validChannel.Pid != 2 || validChannel.ChannelName != "Sample\\sChannel\\sName" || validChannel.TotalClients != 4 || validChannel.ChannelNeededSubscribePower != 5 {
-			t.Errorf("NewChannel(\"%v\"): Parsed version %v does not match source input", validChannelString, validChannel)
+			t.Errorf("NewChannel(\"%v\"): Parsed version %v does not match source input", validChannelPropertyString, validChannel)
+		}
+	}
+
+	// Test to see if a valid channel string is converted into a Channel struct, (with null values)
+	validNullParamChannel, err := NewChannel(validChannelPropertyStringWithNull)
+	if err != nil {
+		t.Errorf("NewChannel(\"%v\"): Errored out with %v", validChannelPropertyStringWithNull, err)
+	} else {
+		if validNullParamChannel.Cid != 1 || validNullParamChannel.Pid != 0 || validNullParamChannel.ChannelName != "Sample\\sChannel\\sName" || validNullParamChannel.TotalClients != 4 || validNullParamChannel.ChannelNeededSubscribePower != 5 {
+			t.Errorf("NewChannel(\"%v\"): Parsed version %v does not match source input", validChannelPropertyStringWithNull, validNullParamChannel)
 		}
 	}
 
 	// Test to see if a invalid channel string throws an error
-	invalidChannel, err := NewChannel(invalidChannelString)
+	invalidChannel, err := NewChannel(invalidChannelPropertyString)
 	if err == nil {
-		t.Errorf("NewChannel(\"%v\"): Should have thrown an error. Instead received %v", invalidChannelString, invalidChannel)
+		t.Errorf("NewChannel(\"%v\"): Should have thrown an error. Instead received %v", invalidChannelPropertyString, invalidChannel)
+	}
+}
+
+func TestDeserialize(t *testing.T) {
+	const validChannelUpdateString = "cid=2 pid=3"
+	const invalidChannelUpdateString = "cid=4 invalid=true"
+
+	// Test to see if a valid channel string is converted into a Channel struct
+	validChannel, err := NewChannel(validChannelPropertyString)
+	if err != nil {
+		t.Errorf("NewChannel(\"%v\"): Errored out with %v", validChannelPropertyString, err)
+	}
+
+	// Test a valid update call
+	_, err = validChannel.Deserialize(validChannelUpdateString)
+
+	if err != nil {
+		t.Errorf("channel.Deserialize(\"%v\"): Errored out with %v", validChannelUpdateString, err)
+	}
+
+	// Test an invalid update call
+	_, err = validChannel.Deserialize(invalidChannelUpdateString)
+
+	if err == nil {
+		t.Errorf("channel.Deserialize(\"%v\"): should have thrown an error", invalidChannelUpdateString, err)
 	}
 }
